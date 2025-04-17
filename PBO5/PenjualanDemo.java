418d286319333c18b86327e379b81aefad392601
Zirfaa <zirlydaffa321@gmail.com>
2025-04-17 16:40:20 +0700

    add PBO5

---
+++ PBO5/PenjualanDemo.java
@@ -0,0 +1,50 @@
+package PBO5;
+class Penjualan{
+    private String nama;
+    private int jumlah;
+    private String kode;
+    private float harga;
+
+    public void setData(String nama, int jumlah,String kode,float harga){
+        this.nama = nama;
+        this.harga = harga;
+        this.kode = kode;
+        this.jumlah = jumlah;
+    }
+    public float getPembelian(){
+        return harga*jumlah;
+    }
+    public String getBonus(){
+        if(getPembelian() >= 500000 && jumlah > 5){
+            return "Mendapatkan Setrika";
+        }
+        else if(getPembelian() >= 100000 && jumlah > 3){
+            return "Mendapatkan Payung";
+        }
+    
+        else if(getPembelian() >= 50000 && jumlah > 2){
+            return "Mendapatkan Ballpoint";
+        }
+        else{
+            return "Anda tidak dapat bonus";
+        }
+    }
+    public void cetakNota(){
+        System.out.println("Nota Pembelian");
+        System.out.println("kode Barang : " + kode);
+        System.out.println("Nama Barang : " + nama);
+        System.out.println("Jumlah Barang : " + jumlah);
+        System.out.println("Harga Barang : "+ harga);
+        System.out.println("Jumlah Pembelian : " + getPembelian());
+        System.out.println("Bonus anda : " + getBonus());
+        System.out.println("Nota Pembelian");
+    }
+}
+
+public class PenjualanDemo {
+    public static void main(String[] args) {
+        Penjualan penjualan1 = new Penjualan();
+        penjualan1.setData("Sandal", 20, "P01", 25000);
+        penjualan1.cetakNota();
+    }
+}
---
+++ PBO5/Rumusabc.java
@@ -0,0 +1,33 @@
+package PBO5;
+import java.util.Scanner;
+
+public class Rumusabc {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukan Nilai a : ");
+        int nilaia = input.nextInt();
+        System.out.print("Masukan Nilai a : ");
+        int nilaib = input.nextInt();
+        System.out.print("Masukan Nilai a : ");
+        int nilaic = input.nextInt();
+
+        int determinan = (nilaib*nilaib) - 4*nilaia*nilaic;
+        System.out.println(determinan);
+        if(determinan > 0){
+            double hasilx1 = -nilaib+Math.sqrt(determinan)/2*nilaia;
+            double hasilx2 = -nilaib-Math.sqrt(determinan)/2*nilaia;
+            System.out.println("Hasil X1 : " + hasilx1 + " dan X2 : " + hasilx2);
+        }
+        if(determinan == 0){
+            double hasil = -nilaib/2*nilaia;
+            System.out.println("Hasil X : " + hasil);
+        }
+        if(determinan < 0){
+            double hasilx1 = (-nilaib/2*nilaia)+(Math.sqrt(-determinan)/2*nilaia);
+            double hasilx2 = (-nilaib/2*nilaia)-(Math.sqrt(-determinan)/2*nilaia);
+            System.out.println("Hasil X1 imajiner : " + hasilx1 + "i " + " dan X2 imanjiner : " + hasilx2 + "i");
+        }
+    }
+}
