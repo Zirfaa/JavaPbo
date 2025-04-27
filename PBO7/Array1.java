990ad6796e60cfc1531e6c4092d99f4602f99d66
Zirfaa <zirlydaffa321@gmail.com>
2025-04-27 17:08:05 +0700

    add Array

---
+++ PBO7/Array1.java
@@ -0,0 +1,15 @@
+package PBO7;
+public class Array1 {
+    
+
+    public static void main(String[] args) {
+        int[] array={68,7,28,90,32};
+        System.out.println("Jumlah bilangan = 5");
+        System.out.println("bilangan ke 1 = " + array[0]);
+        System.out.println("bilangan ke 2 = " + array[1]);
+        System.out.println("bilangan ke 3 = " + array[2]);
+        System.out.println("bilangan ke 4 = " + array[3]);
+        System.out.println("bilangan ke 5 = " + array[4]);
+
+    }
+}
